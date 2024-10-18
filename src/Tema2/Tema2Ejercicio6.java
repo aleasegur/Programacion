4866b144eb0ece612e2186f7aa432c5e87a90022
Alejandro <aasenciogurau@gmail.com>
2024-10-18 19:01:40 +0200

    ad ejer6

---
+++ src/Tema2/Tema2Ejercicio6.java
@@ -0,0 +1,24 @@
+package Tema2;
+
+import java.util.Scanner;
+
+/*ALEJANDRO ASENCIO GURAU
+* Realiza un programa que determina si el número leído por consola es
+positivo, negativo o zero.
+*/
+public class Tema2Ejercicio6 {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduce un valor: ");
+        num=sc.nextInt();
+        if (num==0){
+            System.out.println("El numero es zero");
+        } else if (num>0) {
+            System.out.println("El numero es positivo");
+        }else {
+            System.out.println("El numero es negativo");
+        }
+
+    }
+}
