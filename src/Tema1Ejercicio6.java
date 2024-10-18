e5f9aa0497cc2cba7b2d5cf1207cbdd0df4c927b
aleasegur <aasenciogurau@gmail.com>
2024-10-18 12:11:24 +0200

    cambiar Condiciones

--- src/Tema1Ejercicio6.java
+++ src/Tema1Ejercicio6.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-
+/*Realiza una nueva clase en Java en el proyecto Programacion llamada
+Tema1Ejercicio6 que calcule el área de un círculo. Debe seguir el
+esquema del siguiente ordinograma.*/
 public class Tema1Ejercicio6 {
     public static void main(String[] args) {
-        double radio,area;
-
-        Scanner sc=new Scanner(System.in);
-
+        double radio, area;
+        Scanner sc = new Scanner(System.in);
         System.out.println("Introduce el valor del radio: ");
-        radio=sc.nextDouble();
-        area=Math.PI*radio*radio;
-        System.out.println("El area del circulo es "+area);
+        radio = sc.nextDouble();
+        area = Math.PI * radio * radio;
+        System.out.println("El area del circulo es " + area);
     }
 }
--- src/Tema1Ejercicio7.java
+++ src/Tema1Ejercicio7.java
@@ -6,22 +6,23 @@ correcto si el número es mayor a 6 o menor que 1 y si el número es
 válido escribe por pantalla la cara opuesta del dado*/
 
 import java.util.Scanner;
+
 public class Tema1Ejercicio7 {
     public static void main(String[] args) {
-        int caraDado,caraOpuesta=7;
+        int caraDado, caraOpuesta = 7;
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Introduce un numero de un dado de 6 caras");
-        caraDado=sc.nextInt();
+        caraDado = sc.nextInt();
 
-        if (caraDado<1 || caraDado>6){
-            System.out.println("Error solo es valido del 1 al 6");
-        }else {
-            caraOpuesta-=caraDado;
+        if (caraDado >= 1 && caraDado <= 6) {
+            caraOpuesta -= caraDado;
+        } else {
+            System.err.println("Error solo es valido del 1 al 6");
         }
 
-        System.out.println("La cara opuesta de "+caraDado+" es "+caraOpuesta);
+        System.out.println("La cara opuesta de " + caraDado + " es " + caraOpuesta);
 
     }
 }
