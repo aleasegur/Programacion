7cb71fe3f4988ed5d3ec688cbc4ca7c2fd9b1ef5
aleasegur <aasenciogurau@gmail.com>
2024-10-18 13:57:20 +0200

    Add ejercicios 3 y 4

---
+++ src/Tema2/Tema2Ejercicio3.java
@@ -0,0 +1,20 @@
+package Tema2;
+
+import java.util.Scanner;
+
+/*ALEJANDRO ASENCIO GURAU
+Realiza un programa que lea por consola la base y la altura de un
+triángulo y mostrará por consola la superficie.*/
+public class Tema2Ejercicio3 {
+    public static void main(String[] args) {
+        double base,alt,sup=0.0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduce la base: ");
+        base=sc.nextDouble();
+        System.out.println("Introduce la altura: ");
+        alt=sc.nextDouble();
+        sup=base*alt/2;
+        System.out.println("---TRIANGULO--- "+"\n"+
+                "Superficie = "+sup);
+    }
+}
---
+++ src/Tema2/Tema2Ejercicio4.java
@@ -0,0 +1,27 @@
+package Tema2;
+
+import java.util.Scanner;
+
+/*ALEJANDRO ASENCIO GURAU
+* Realiza un programa que lea 3 números por consola y devuelva el
+mayor de ellos.*/
+public class Tema2Ejercicio4 {
+    public static void main(String[] args) {
+        double num1,num2,num3;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduce el primer valor: ");
+        num1= sc.nextDouble();
+        System.out.println("Introduce el segundo valor: ");
+        num2= sc.nextDouble();
+        System.out.println("Introduce el tercer valor: ");
+        num3= sc.nextDouble();
+
+        if (num1>num2 && num1>num3){
+            System.out.println("El mayor es "+num1);
+        }else if (num2>num1 && num2>num3){
+            System.out.println("El mayor es "+num2);
+        }else {
+            System.out.println("El mayor es "+num3);
+        }
+    }
+}
