941a9237c28ed36b15f04a8b8b48a3663091502f
aleasegur <aasenciogurau@gmail.com>
2024-10-18 13:44:09 +0200

    Add ejersTema2

--- src/Tema2/Tema2Ejercicio1.java
+++ src/Tema2/Tema2Ejercicio1.java
@@ -1,7 +1,20 @@
 package Tema2;
 
+import java.util.Scanner;
+
+/*ALEJANDRO ASENCIO GURAU
+* Realiza un programa que lea por consola el lado de un cuadrado y
+mostrará por consola la superficie y el perímetro.*/
 public class Tema2Ejercicio1 {
     public static void main(String[] args) {
-
+        int lado, peri = 0, sup = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el valor del lado: ");
+        lado = sc.nextInt();
+        sup = lado * lado;
+        peri = lado * 4;
+        System.out.println("----CUADRADO---- " + "\n" +
+                "Superficie = " + sup + "\n" +
+                "Perimetro = " + peri);
     }
 }
---
+++ src/Tema2/Tema2Ejercicio2.java
@@ -0,0 +1,22 @@
+package Tema2;
+
+import java.util.Scanner;
+
+/*ALEJANDRO ASENCIO GURAU
+Realiza un programa que lea por consola la base y la altura de un
+rectángulo y mostrará por consola la superficie y el perímetro.*/
+public class Tema2Ejercicio2 {
+    public static void main(String[] args) {
+        double base,alt,per=0.0,sup=0.0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduce el valor de la base: ");
+        base=sc.nextDouble();
+        System.out.println("Introduce el valor de la altura: ");
+        alt=sc.nextDouble();
+        sup=base*alt;
+        per=2*(base+alt);
+        System.out.println("---RECTANGULO--- "+"\n"+
+                "Superficie = "+sup+"\n"+
+                "Perimetro = "+per);
+    }
+}
