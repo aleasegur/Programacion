41603cf2eaec2b744e947a332af4498900bbe8b9
aleasegur <aasenciogurau@gmail.com>
2024-10-21 09:03:44 +0200

    cambios en ejer15,17

--- src/Tema2/Tema2Ejercicio17.java
+++ src/Tema2/Tema2Ejercicio17.java
@@ -1,15 +1,19 @@
 package Tema2;
+
+import java.util.Scanner;
+
 /*ALEJANDRO ASENCIO GURAU
 * Realiza un programa que si lee por consola 0, muestra la superficie y el
 perímetro de un cuadrado; si lee 1, muestra la superficie y el perímetro
 de un rectángulo; y si lee 2, muestra la superficie de un triángulo. Todos
 los valores se leen por consola. Si no es una opción válida se indica por
-consola que ha habido un error.
-18. Realiza un programa que calcula la raíz cuadrada de un número. Si el
-número es negativo debe mostrar un mensaje de error.
-Intenta no usar "break"*/
+consola que ha habido un error.*/
 public class Tema2Ejercicio17 {
     public static void main(String[] args) {
+        int opcion;
+        double num1,num2,sup,perim;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("---SUPERFICIE/PERIMETRO---");
 
     }
 }
