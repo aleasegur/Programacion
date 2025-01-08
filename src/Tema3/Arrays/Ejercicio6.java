d4d45bee228f9f39188e6aee2d71c8ca3359bbd1
Alejandro <aasenciogurau@gmail.com>
2025-01-08 10:23:31 +0100

    add ejer6

---
+++ src/Tema3/Arrays/Ejercicio6.java
@@ -0,0 +1,16 @@
+package Tema3.Arrays;
+/*ALEJANDRO ASENCIO GURAU
+*  Realiza el juego “Buscaminas” en un array. Hay un array de 20 elementos
+ (no visible por el jugador en un principio) con 6 minas ubicadas
+ aleatoriamente. A continuación se rellena el resto de las casillas del array con
+ las pistas: 1 si tiene una mina alrededor, 2 si tiene dos y 0 si no tiene ninguna.
+ Por ejemplo:
+ 0 1 * * * 2 * 0 0 0 0 0 1 * 1 0 0 0 1 *
+ El jugador selecciona una posición. Si hay una mina, pierde la partida. Si hay
+ un número se muestra dicha posición. Si se muestra todo el tablero menos las
+ minas el jugador gana la partida*/
+public class Ejercicio6 {
+    public static void main(String[] args) {
+
+    }
+}
