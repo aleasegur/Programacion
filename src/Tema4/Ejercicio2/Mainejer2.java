cafe4b61952181cee16aec5170c529fac5aa5109
Alejandro <aasenciogurau@gmail.com>
2025-01-21 21:18:02 +0100

    add Mainejer2.java y Persona.java, methods and constructor en Persona.java, ver si hay que hacer algun cambio

---
+++ src/Tema4/Ejercicio2/Mainejer2.java
@@ -0,0 +1,58 @@
+package Tema4.Ejercicio2;
+
+import java.util.Scanner;
+
+public class Mainejer2 {
+    public static Persona crearPersona(Scanner sc){
+        int edad;
+        String dni,nombre,apellidos;
+
+        System.out.println("Introduce tu nombre: ");
+        nombre=sc.next();
+        System.out.println("Introduce tus apellidos: ");
+        apellidos=sc.next();
+
+        do {
+            System.out.println("Introduce tu edad: ");
+            edad = sc.nextInt();
+        }while (!edadNoNegativo(edad));
+
+        do {
+            System.out.println("Introduce tu dni: ");
+            dni = sc.next().toUpperCase();
+        }while (!Persona.checkDNI(dni));
+
+        return new Persona(nombre,apellidos,dni,edad);
+    }
+
+    public static boolean edadNoNegativo(int edad){
+        boolean res=true;
+        if (edad<=0){
+            System.err.println("La edad no puede ser menor o igual a 0");
+            res=false;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Persona persona1=crearPersona(sc);
+        Persona persona2=crearPersona(sc);
+
+        int diferenciaEdad;
+
+        System.out.println("---DATOS//PRIMERA PERSONA---");
+        persona1.print();
+        System.out.println("Es mayor de edad "+persona1.isAdult());
+        System.out.println("Esta jubilado "+persona1.isRetired());
+
+        System.out.println("---DATOS//SEGUNDA PERSONA");
+        persona2.print();
+        System.out.println("Es mayor de edad "+persona2.isAdult());
+        System.out.println("Esta jubilado "+persona2.isAdult());
+
+        diferenciaEdad= persona1.ageDifference(persona2);
+        System.out.println("La edad de diferencia entre las dos persoanas es de "+diferenciaEdad);
+
+    }
+}
---
+++ src/Tema4/Ejercicio2/Persona.java
@@ -0,0 +1,109 @@
+package Tema4.Ejercicio2;
+
+/* ALEJANDRO ASENCIO GURAU
+*  2. Crea un programa principal y una clase Persona que representará los datos
+ principales de una persona: dni, nombre, apellidos y edad.
+ En el main de la clase principal pide por teclado los datos de 2 personas y
+ guárdalos en dos objetos de la clase Persona y utiliza todos los métodos que
+ crearemos a continuación.
+ Métodos / atributos a implementar:----------
+ Implementa un constructor con parámetros.
+ Implementar getters y setters.
+ print(), muestra por pantalla la información de la persona.
+ isAdult(), indica si es mayor de edad o no.
+ isRetired(), indica si está jubilado o no (>65 años).
+ ageDifference(), recibe como parámetro otra persona e indica la diferencia de
+ edad.
+ haz que el dni sea un atributo que no se pueda modificar (final).
+ crea un atributo adultAge final y static que valga 18. Utilízalo.
+ crea un atributo retiredAge final y static que valga 65. Utilízalo.
+ checkDNI(), será un método estático que devuelve si el dni es correcto o no*/
+public class Persona {
+    final static int adultAge = 18;
+    final static int retiredAge = 65;
+    private final String dni;
+    private String nombre, apellidos;
+    private int edad;
+
+    public Persona(String nombre, String apellidos, String dni, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void print() {
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellidos: "+apellidos);
+        System.out.println("DNI: "+dni);
+        System.out.println("Edad: "+edad);
+    }
+
+    public boolean isAdult(){
+        boolean res=false;
+        if (this.edad>=adultAge){
+            System.out.println("Es mayor de edad");
+            res=true;
+        }
+        return res;
+    }
+
+    public boolean isRetired(){
+        boolean res=false;
+        if (this.edad>retiredAge){
+            res=true;
+        }
+        return res;
+    }
+
+    public int ageDifference(Persona otraPersona){
+        int res;
+        res=Math.abs(this.edad-otraPersona.getEdad());
+        return res;
+    }
+
+    public static boolean checkDNI(String dni){
+        boolean res=true;
+        char caracterUltimo=dni.toUpperCase().charAt(8);
+
+        if (dni.length()!=9){
+            System.err.println("El DNI debe tener 8 digitos");
+            res=false;
+        }
+
+        if (!Character.isLetter(caracterUltimo)){
+            System.err.println("El DNI debe tener una letra");
+            res=false;
+        }
+        return res;
+    }
+
+}
