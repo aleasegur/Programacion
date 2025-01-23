58c2324b396cd4a92acdbed49df141a7c5bb980c
Alejandro <aasenciogurau@gmail.com>
2025-01-23 20:30:44 +0100

    block code de prueba, comprobar mañana

--- src/Tema4/Ejercicio4/MainCombatEjer4.java
+++ src/Tema4/Ejercicio4/MainCombatEjer4.java
@@ -3,6 +3,10 @@ import Tema4.Ejercicio3.Hero;
 public class MainCombatEjer4 {
     public static void main(String[] args) {
         Hero hero=new Hero();
-        System.out.println(hero);
+        Hero hero2=new Hero();
+        Hero[] enemigos=new Hero[]{hero,hero2};
+        for (Hero enemigo : enemigos) {
+            System.out.println(enemigo);
+        }
     }
 }
