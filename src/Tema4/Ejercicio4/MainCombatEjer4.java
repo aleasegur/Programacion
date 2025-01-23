c54d309815bec86aaa27a2336b371882cf5a5270
Alejandro <aasenciogurau@gmail.com>
2025-01-23 20:21:53 +0100

    add MainCombatEjer4.java

---
+++ src/Tema4/Ejercicio4/MainCombatEjer4.java
@@ -0,0 +1,8 @@
+package Tema4.Ejercicio4;
+import Tema4.Ejercicio3.Hero;
+public class MainCombatEjer4 {
+    public static void main(String[] args) {
+        Hero hero=new Hero();
+        System.out.println(hero);
+    }
+}
