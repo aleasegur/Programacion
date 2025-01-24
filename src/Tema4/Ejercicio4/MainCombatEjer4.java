7c103181eebe5e2ba263997887f9ddc1d305c729
Alejandro <aasenciogurau@gmail.com>
2025-01-24 16:10:54 +0100

    Correcion al bucle infinito(no lo era),ya que los atributos era demasiado fuerte y entonces generaba un combate sin floritura

--- src/Tema4/Ejercicio4/MainCombatEjer4.java
+++ src/Tema4/Ejercicio4/MainCombatEjer4.java
@@ -8,79 +8,97 @@ import java.util.Scanner;
 public class MainCombatEjer4 {
     public static boolean enemigosVivos(Hero[] enemigos) {
         for (Hero enemigo : enemigos) {
-            if (enemigo != null && enemigo.getHealth() > 0) {
-                return true; // Si encuentra al menos un enemigo vivo, retorna true
+            if (enemigo.getHealth() > 0) {
+                return true;
             }
         }
-        return false; // Si no hay enemigos vivos, retorna false
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        int ataque1, ataque2, ronda = 1;
         Hero hero = Mainejer3.crearHeroe(sc);
+        int ataque1, ataque2, ronda = 1;
+        boolean combateHeroe=true;
 
-        while (hero.getHealth() > 0) {
+        while (combateHeroe) {
             int numEnemigos = rand.nextInt(3) + 1;
             Hero[] enemigos = new Hero[numEnemigos];
 
             // Crear enemigos
             for (int i = 0; i < numEnemigos; i++) {
-                enemigos[i] = new Hero("Enemigo " + (i + 1), rand.nextInt(50) + 50,
-                        rand.nextInt(15) + 5,
-                        rand.nextInt(10) + 5);
+                enemigos[i] = new Hero("Bandido " + (i + 1), rand.nextInt(50) + 50 +ronda *10,
+                        rand.nextInt(15) + 5 +ronda*2,
+                        rand.nextInt(10) + 5+ronda*2);
             }
 
             System.out.println("Horda " + ronda + " con " + numEnemigos + " enemigos aparece!");
 
-            // Combate con la horda
-            while (hero.getHealth() > 0 && enemigosVivos(enemigos)) {
-                // Ataques del héroe
-                for (int i = 0; i < enemigos.length; i++) {
-                    if (enemigos[i] != null && enemigos[i].getHealth() > 0) {
-                        ataque1 = hero.attack(enemigos[i]);
-                        System.out.println(hero.getName() + " atacó a " + enemigos[i].getName() + " causando " + ataque1 + " de daño");
+            boolean combateHorda=true;
 
-                        if (enemigos[i].getHealth() <= 0) {
-                            System.out.println(enemigos[i].getName() + " ha sido derrotado!");
-                            enemigos[i] = null; // El enemigo es eliminado
+            // Combate con la horda
+            while (combateHorda) {
+                System.out.println("---BATTLE---");
+                // Ataques del heroe
+                for (Hero value : enemigos) {
+                    if (value.getHealth() > 0) {
+                        ataque1 = hero.attack(value);
+                        System.out.println(hero.getName() + " atacó a " + value.getName() + " causando " + ataque1 + " de daño");
+
+                        if (value.getHealth() <= 0) {
+                            System.out.println("---DEFEAT---");
+                            System.out.println(value.getName() + " ha sido derrotado!");
+
+                            //10% de probabilidad de huir de la pelea.
                         } else if (rand.nextInt(100) < 10) {
-                            System.out.println(enemigos[i].getName() + " huyó de la pelea!");
-                            enemigos[i] = null; // El enemigo huye
+                            System.out.println("---COWARD---");
+                            System.out.println(value.getName() + " huyó de la pelea!");
                         }
                     }
                 }
 
-                // Ataques de los enemigos
+                // Ataque de los enemigos
                 for (Hero enemigo : enemigos) {
-                    if (enemigo != null && enemigo.getHealth() > 0) {
+                    if (enemigo.getHealth() > 0) {
                         ataque2 = enemigo.attack(hero);
                         System.out.println(enemigo.getName() + " atacó a " + hero.getName() + " causando " + ataque2 + " de daño");
 
                         if (hero.getHealth() <= Hero.MIN_NUMBER_LIMIT) {
+                            System.out.println("---WASTED---");
                             System.out.println(hero.getName() + " ha sido derrotado");
-                            break; // Salir del combate si el héroe muere
+                            combateHeroe=false;
+                            combateHorda=false;
                         }
                     }
                 }
+                if (!enemigosVivos(enemigos)){
+                    System.out.println("---WIN---");
+                    System.out.println("Los enemigos han sido derrotados!");
+                    combateHorda=false;
+                }
             }
 
-            // Fin de la horda
-            if (hero.getHealth() > 0) {
-                ronda++;
-                System.out.println("Has sobrevivido a la horda " + ronda);
 
-                // Probabilidad de descansar o encontrar una poción
+            System.out.println("-------------------------------");
+
+            if (hero.getHealth() > 0) {
+                System.out.println("\n"+hero.getName()+" ha sobrevivido a la horda " + ronda);
+
+                // 0.1% de probabilidad de descansar y recuperar vida
                 if (rand.nextInt(1000) < 1) {
                     int descansar = hero.rest();
+                    System.out.println("---DEFEAT---");System.out.println("---DEFEAT---");
                     System.out.println(hero.getName() + " ha descansado y recuperado " + descansar + " de vida");
                 }
 
+                //10 % de probabilidad de encontrar una poción y recuperar vida.
                 if (rand.nextInt(100) < 1) {
                     int pocion = hero.drinkPotion();
+                    System.out.println("---DRINK---");
                     System.out.println(hero.getName() + " ha encontrado una poción y recupera " + pocion + " de vida");
                 }
+                ronda++;
             }
         }
 
