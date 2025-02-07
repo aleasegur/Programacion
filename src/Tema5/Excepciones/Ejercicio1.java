85ac9e2321ebdaa2a05a485ca3624b9d8c52b859
aleasegur <aasenciogurau@gmail.com>
2025-02-07 13:57:10 +0100

    add ejercicios 1 y 2

---
+++ src/Tema5/Excepciones/Ejercicio1.java
@@ -0,0 +1,32 @@
+package Tema5.Excepciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*ALEJANDRO ASENCIO GURAU
+* Implementa un programa que pida al usuario un valor entero num utilizando
+un nextInt( ) (de Scanner) y luego muestre por pantalla el mensaje “Valor
+introducido: ...”. Se deberá tratar la excepción InputMismatchException que
+lanza nextInt( ) cuando no se introduce un entero válido. En tal caso se mostrará
+el mensaje “Valor introducido incorrecto”*/
+public class Ejercicio1 {
+
+    public static int introducirNumero(Scanner sc){
+        int num=0;
+        try {
+            System.out.println("Introduce un numero entero: ");
+            num = sc.nextInt();
+        }catch (InputMismatchException iE){
+            System.err.println("Valor introducido incorrecto");
+            //iE.printStackTrace();
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int resultNum=introducirNumero(sc);
+        System.out.println("Valor introducido: "+resultNum);
+        sc.close();
+    }
+}
---
+++ src/Tema5/Excepciones/Ejercicio2.java
@@ -0,0 +1,12 @@
+package Tema5.Excepciones;
+/*ALEJANDRO ASENCIO GURAU
+* Implementa un programa que pida dos valores int num1 y num2 utilizando un
+nextInt( ) (de Scanner), calcule num1/num2 y muestre el resultado por pantalla.
+Se deberán tratar de forma independiente las dos posibles excepciones,
+InputMismatchException y ArithmeticException, mostrando en cada caso un
+mensaje de error diferente en cada caso*/
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+    }
+}
