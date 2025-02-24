cf14b52e075072db41b7a491e74d0cb77a829465
aleasegur <aasenciogurau@gmail.com>
2025-02-24 10:02:24 +0100

    add main con iLanzar()

--- src/Tema6/Ejercicio8/PruebaAtaqueAvanzadeo.java
+++ src/Tema6/Ejercicio8/PruebaAtaqueAvanzadeo.java
@@ -1,4 +1,26 @@
 package Tema6.Ejercicio8;
 
+import java.util.InputMismatchException;
+
 public class PruebaAtaqueAvanzadeo {
+    public static void main(String[] args) {
+        int fuerza,poderMag;
+        try {
+            poderMag =15;
+            fuerza=20;
+            AtaqueBolaFuego ataqueBolaFuego=new AtaqueBolaFuego(poderMag);
+            AtaqueEspadazo ataqueEspadazo=new AtaqueEspadazo(fuerza);
+            AtaqueEspadazoEncantado ataqueEspadazoEncantado=new AtaqueEspadazoEncantado(poderMag,fuerza);
+            System.out.println("---ATAQUE BOLA DE FUEGO---");
+            ataqueBolaFuego.iLanzar();
+            System.out.println("---ATAQUE ESPADAZO---");
+            ataqueEspadazo.iLanzar();
+            System.out.println("---ATAQUE ESPADAZO ENCANTADO---");
+            ataqueEspadazoEncantado.iLanzar();
+        }catch (InputMismatchException | ArithmeticException e){
+            System.err.println(e.getMessage());
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
 }
