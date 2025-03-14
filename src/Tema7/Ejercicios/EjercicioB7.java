f12597cae5a57a9511532394714ee51045784cf3
aleasegur <aasenciogurau@gmail.com>
2025-03-14 12:46:17 +0100

    add EjercicioB7.java

---
+++ src/Tema7/Ejercicios/EjercicioB7.java
@@ -0,0 +1,48 @@
+package Tema7.Ejercicios;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class EjercicioB7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int numLineas=0,numPalabras=0,numChars=0;
+        String fileName=MyMethodsAtributes.introducirCadena(sc,"el nombre del archivo a analizar(ej: coplas_manrique.txt): ");
+        Map<String,Integer> mapEstadistica=new Hashtable<>();
+
+        try(BufferedReader br=new BufferedReader(new FileReader(MyMethodsAtributes.namePathBooks+fileName))) {
+            String linea;
+            while ((linea=br.readLine())!=null){
+                numLineas++;
+                numChars+=linea.length();
+                String[] palabras=linea.replaceAll("[^a-zA-Záéíóúüñ]]","").split("\\s+");
+                numPalabras+=palabras.length;
+
+                for (String palabra : palabras){
+                    if (!palabra.isEmpty()){
+                        mapEstadistica.put(palabra,mapEstadistica.getOrDefault(palabra,0)+1);
+                    }
+                }
+            }
+
+            List<Map.Entry<String,Integer>> listPalabras=new ArrayList<>(mapEstadistica.entrySet());
+            listPalabras.sort((a,b)-> b.getValue().compareTo(a.getValue()));
+            System.out.println("Numero de lineas "+numLineas);
+            System.out.println("Numero de palabras "+numPalabras);
+            System.out.println("Numero de caracteres "+numChars);
+
+            int cont=0;
+            for (Map.Entry<String,Integer>entry : listPalabras){
+                cont++;
+                if (cont<10){
+                    System.out.println(entry.getKey()+" : "+entry.getValue());
+                }
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
