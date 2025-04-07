074075f8a1e6df402c2c11ec85a56b075646815d
aleasegur <aasenciogurau@gmail.com>
2025-04-07 09:55:51 +0200

    add metodos

---
+++ src/RankingVideojuegos/MetodosRanking.java
@@ -0,0 +1,76 @@
+package RankingVideojuegos;
+
+import javax.print.DocFlavor;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MetodosRanking {
+    public static final String DIRECTORIO="DirectorioRankingGames/";
+    public static final String DIR_FILE_TXT="DirectorioRankingGames/ranking.txt";
+    public static final String DIR_FILE_DAT="DirectorioRankingGames/ranking.dat";
+
+    public static String introducirString(Scanner sc,String tipo){
+        String res="";
+        try {
+            System.out.println("Introduce "+tipo);
+            res=sc.next().toLowerCase();
+        }catch (StringIndexOutOfBoundsException e){
+            System.err.println(e.getMessage());
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+        return res;
+    }
+
+    public static int introdcirNota(Scanner sc, String tipo){
+        int res=0;
+        do {
+            try {
+                System.out.println("Introduce " + tipo);
+                res = sc.nextInt();
+
+            } catch (InputMismatchException | ArithmeticException e) {
+                System.err.println(e.getMessage());
+            }catch (Exception e){
+                System.err.println(e.getMessage());
+            }
+        }while (res<0 || res>10);
+        return res;
+    }
+
+    public static double introdcirTam(Scanner sc, String tipo){
+        double res=0;
+        do {
+            try {
+                System.out.println("Introduce " + tipo);
+                res = sc.nextDouble();
+            } catch (InputMismatchException | ArithmeticException e) {
+                System.err.println(e.getMessage());
+            }catch (Exception e){
+                System.err.println(e.getMessage());
+            }
+        }while (res<0 || res>10);
+        return res;
+    }
+
+    public static String introducirEstado(Scanner sc,String tipo){
+        String res;
+        boolean bucleEstado=true;
+        int opcion;
+        while (bucleEstado){
+            try {
+                System.out.println("---SELECCIONA EL ESTADO DEL VIDEOJUEGO FISICO---");
+                System.out.println("1.Nuevo");
+                System.out.println("2.Usado");
+                System.out.println("Elegir opcion: ");
+                opcion=sc.nextInt();
+
+            }catch (InputMismatchException | ArithmeticException e){
+                System.err.println(e.getMessage());
+            }catch (Exception e){
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+}
