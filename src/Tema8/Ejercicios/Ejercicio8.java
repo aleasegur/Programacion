4c516fcb44f9d219c552b68151fde19115bfb41f
Alejandro <aasenciogurau@gmail.com>
2025-04-29 10:25:57 +0200

    add ejer8

--- src/Tema8/Ejercicios/Ejercicio8.java
+++ src/Tema8/Ejercicios/Ejercicio8.java
@@ -3,18 +3,20 @@ package Tema8.Ejercicios;
 import Tema8.MyDatos;
 
 import java.sql.*;
-/*7. Filtrado de patrones con LIKE
-Selecciona los nombres de los estudiantes cuyo apellido empieza con la letra "P"*/
-public class Ejercicio7 {
+
+/*8. Uso de IN para búsqueda múltiple
+Muestra los nombres y apellidos de los estudiantes que están en su 4º o 5º año.*/
+public class Ejercicio8 {
     public static void main(String[] args) {
         MyDatos.loadProperties();
-        String consulta="Select nombre FROM estudiante WHERE apellido LIKE 'P%'";
+        String consulta="SELECT nombre,apellido,anyo_curso FROM estudiante WHERE anyo_curso IN (4,5)";
         try(Connection con= DriverManager.getConnection(MyDatos.getURL(),MyDatos.getUSER(),MyDatos.getPASSWORD()); PreparedStatement sta=con.prepareStatement(consulta);
             ResultSet res=sta.executeQuery()) {
-            System.out.println("---ESTUDIANTES QUE SU APELLIDO EMPIEZA POR LA P---");
             while (res.next()){
                 String nombre=res.getString("nombre");
-                System.out.println(nombre);
+                String apellido=res.getString("apellido");
+                int anyo=res.getInt("anyo_curso");
+                System.out.println(nombre + " " + apellido+", Curso: "+anyo);
             }
         }catch (SQLException e){
             System.err.println(e.getMessage());
