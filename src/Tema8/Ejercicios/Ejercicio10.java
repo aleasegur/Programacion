6a6f38cc27b84e17533051a591d8082736014991
Alejandro <aasenciogurau@gmail.com>
2025-04-29 10:37:18 +0200

    add ejer10

---
+++ src/Tema8/Ejercicios/Ejercicio10.java
@@ -0,0 +1,29 @@
+package Tema8.Ejercicios;
+
+import Tema8.MyDatos;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+/*10.Limitar resultados con LIMIT
+Muestra los primeros 5 estudiantes ordenados por su fecha de nacimiento*/
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        MyDatos.loadProperties();
+        String consulta="SELECT nombre, apellido, fecha_nacimiento " +
+                "FROM estudiante " +
+                "ORDER BY fecha_nacimiento ASC " +
+                "LIMIT 5";
+        try(Connection con= DriverManager.getConnection(MyDatos.getURL(),MyDatos.getUSER(),MyDatos.getPASSWORD()); PreparedStatement sta=con.prepareStatement(consulta);
+            ResultSet res=sta.executeQuery()) {
+            while (res.next()){
+                String nombre=res.getString("nombre");
+                String apellido=res.getString("apellido");
+                LocalDate date=res.getObject("fecha_nacimiento",LocalDate.class);
+                System.out.println(nombre+" "+apellido+", Fecha: "+date);
+            }
+        }catch (SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
