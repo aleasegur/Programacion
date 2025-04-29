6c97d63af4439f3ef3c76a7bb8f7bb14c5a6d618
Alejandro <aasenciogurau@gmail.com>
2025-04-29 10:15:30 +0200

    add ejer7

---
+++ src/Tema8/Ejercicios/Ejercicio7.java
@@ -0,0 +1,22 @@
+package Tema8.Ejercicios;
+
+import Tema8.MyDatos;
+
+import java.sql.*;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        MyDatos.loadProperties();
+        String consulta="Select nombre FROM estudiante WHERE apellido LIKE 'P%'";
+        try(Connection con= DriverManager.getConnection(MyDatos.getURL(),MyDatos.getUSER(),MyDatos.getPASSWORD()); PreparedStatement sta=con.prepareStatement(consulta);
+            ResultSet res=sta.executeQuery()) {
+            System.out.println("---ESTUDIANTES QUE SU APELLIDO EMPIEZA POR LA P---");
+            while (res.next()){
+                String nombre=res.getString("nombre");
+                System.out.println(nombre);
+            }
+        }catch (SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
